aa31ba3b2e699eddbc548a54c66ced3d53b80194
makessel <max.kesselbacher@aau.at>
2019-12-09 14:39:22 +0100

    add sonar metrics analysis

--- src/test/java/com/github/mitschi/sedashboard/SonarCommitAnalysisTest.java
+++ src/test/java/com/github/mitschi/sedashboard/SonarCommitAnalysisTest.java
@@ -1,20 +1,32 @@
 package com.github.mitschi.sedashboard;
 
-import static org.junit.Assert.assertTrue;
+import java.io.File;
 
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for SonarCommitAnalysis.
  */
 public class SonarCommitAnalysisTest
 {
     /**
-     * Rigorous Test :-)
+     * Starts sonar metric analysis (start sonar scanner, get metrics from sonar db)
+     * and asserts that there are metrics extracted.
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void SonarMetricAnalyseCommit()
     {
-
+    	File projectSrcRoot = new File("D:\\git\\KAUA\\kaua-server");
+    	File projectBinaryRoot = new File("TODO");
+    	
+    	// TODO: add suitable db driver dependency!
+    	SonarConnectionDetails sonarConnectionDetails = new SonarConnectionDetails("jdbc:h2:tcp://localhost:9092/sonar", 
+    			"" /* user */,
+    			"" /* pwd */);
+    	
+    	SonarCommitAnalysis sca = SonarMetricsAnalyzer.analyzeCommit(projectSrcRoot, projectBinaryRoot, sonarConnectionDetails);
+    	System.out.println(sca.getSonarMetrics());
+    	assert(sca.getSonarMetrics() != null);
+    	assert(sca.getSonarMetrics().size() == 15);
     }
 }
